fe811f5586da979efe8c4d96cb6d2c41f89209b5
antonstenaxel <antonstenaxel@gmail.com>
2019-03-13 17:00:14 +0100

    Updates

---
+++ precache-manifest.fb0e1e96c6d43bff7f5546438497b7e3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "b16c2f8f9478090aca8a610102e5af8e",
+    "url": "/wedding_website/static/media/saturday.b16c2f8f.png"
+  },
+  {
+    "revision": "239772e9d1c251107ca5",
+    "url": "/wedding_website/static/css/main.8563357c.chunk.css"
+  },
+  {
+    "revision": "63cee6de9a735abf141d",
+    "url": "/wedding_website/static/js/runtime~main.3e41b830.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/wedding_website/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "327720915922295efe97",
+    "url": "/wedding_website/static/js/2.d7558446.chunk.js"
+  },
+  {
+    "revision": "696b195ce7f20e12dfc9e7827961fc70",
+    "url": "/wedding_website/static/media/hearts.696b195c.png"
+  },
+  {
+    "revision": "5051d3322e703065a13416ae9532bd7f",
+    "url": "/wedding_website/static/media/surprise.5051d332.png"
+  },
+  {
+    "revision": "6405cfbca8e3cb7c75e8f58ac317be2c",
+    "url": "/wedding_website/static/media/sleep.6405cfbc.png"
+  },
+  {
+    "revision": "af8445541b1396bbfbedcd2fa401ba16",
+    "url": "/wedding_website/static/media/toastmasters.af844554.png"
+  },
+  {
+    "revision": "239772e9d1c251107ca5",
+    "url": "/wedding_website/static/js/main.cdecd8d7.chunk.js"
+  },
+  {
+    "revision": "f09ff199f28da5c8852cb58ba018d847",
+    "url": "/wedding_website/static/media/aah.f09ff199.png"
+  },
+  {
+    "revision": "f5db2a5e9d9213313c78e407102428cf",
+    "url": "/wedding_website/static/media/love.f5db2a5e.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/wedding_website/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/wedding_website/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/wedding_website/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/wedding_website/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "327720915922295efe97",
+    "url": "/wedding_website/static/css/2.082e2b9b.chunk.css"
+  },
+  {
+    "revision": "cdfd7e74c0c8c61166920615ed68abcd",
+    "url": "/wedding_website/index.html"
+  }
+];
